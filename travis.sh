b200a96ba14b907728cee4565838228c89485206
Ralf Jung <post@ralfj.de>
2019-06-14 11:18:42 +0200

    debug cron job failure

--- travis.sh
+++ travis.sh
@@ -6,6 +6,12 @@ rm -rf rust-src-patched
 cp -a $(rustc --print sysroot)/lib/rustlib/src/rust/ rust-src-patched
 ( cd rust-src-patched && patch -f -p1 < ../rust-src.diff )
 
+# debugging
+rustc --version
+rustc --print sysroot
+rustup run $(cat rust-version) miri --version
+rustup run $(cat rust-version) miri --print sysroot
+
 # run the tests (some also without validation, to exercise those code paths in Miri)
 export RUST_SRC=rust-src-patched
 echo && echo "## Testing core (without validation)" && echo
