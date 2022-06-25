aef74c923773cf4f4438a2dec983a79508e62bff
Ralf Jung <post@ralfj.de>
2022-06-24 23:13:56 -0400

    maybe it's a cache thing??

--- ci-test.sh
+++ ci-test.sh
@@ -10,12 +10,12 @@ export MIRI_LIB_SRC=$(pwd)/rust-src-patched/library
 # run the tests (some also without validation, to exercise those code paths in Miri)
 case "$1" in
 core)
-    echo && echo "## Testing core (no validation, no Stacked Borrows, symbolic alignment)" && echo
-    MIRIFLAGS="-Zmiri-disable-validation -Zmiri-disable-stacked-borrows -Zmiri-symbolic-alignment-check" \
-             ./run-test.sh core --lib --tests -- --skip align 2>&1 | ts -i '%.s  '
-    echo && echo "## Testing core (strict provenance)" && echo
-    MIRIFLAGS="-Zmiri-strict-provenance" \
-             ./run-test.sh core --lib --tests 2>&1 | ts -i '%.s  '
+    #echo && echo "## Testing core (no validation, no Stacked Borrows, symbolic alignment)" && echo
+    #MIRIFLAGS="-Zmiri-disable-validation -Zmiri-disable-stacked-borrows -Zmiri-symbolic-alignment-check" \
+    #         ./run-test.sh core --lib --tests -- --skip align 2>&1 | ts -i '%.s  '
+    #echo && echo "## Testing core (strict provenance)" && echo
+    #MIRIFLAGS="-Zmiri-strict-provenance" \
+    #         ./run-test.sh core --lib --tests 2>&1 | ts -i '%.s  '
     # Cannot use strict provenance as there are int-to-ptr casts in the doctests.
     echo && echo "## Testing core docs" && echo
     MIRIFLAGS="-Zmiri-ignore-leaks -Zmiri-disable-isolation" \
