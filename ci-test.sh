86e1acb7a22a042285bc552eaacec1724bf4bde7
Ralf Jung <post@ralfj.de>
2022-06-25 08:40:24 -0400

    debug

--- ci-test.sh
+++ ci-test.sh
@@ -17,34 +17,11 @@ core)
     #MIRIFLAGS="-Zmiri-strict-provenance" \
     #         ./run-test.sh core --lib --tests 2>&1 | ts -i '%.s  '
     # Cannot use strict provenance as there are int-to-ptr casts in the doctests.
+    ./run-test.sh core --doc atomic
     echo && echo "## Testing core docs" && echo
     MIRIFLAGS="-Zmiri-ignore-leaks -Zmiri-disable-isolation" \
              ./run-test.sh core --doc
     ;;
-alloc)
-    echo && echo "## Testing alloc (symbolic alignment, strict provenance)" && echo
-    MIRIFLAGS="-Zmiri-symbolic-alignment-check -Zmiri-strict-provenance" \
-             ./run-test.sh alloc --lib --tests 2>&1 | ts -i '%.s  '
-    echo && echo "## Testing alloc docs (strict provenance)" && echo
-    MIRIFLAGS="-Zmiri-ignore-leaks -Zmiri-disable-isolation -Zmiri-strict-provenance" \
-             ./run-test.sh alloc --doc
-    ;;
-simd)
-    cd $MIRI_LIB_SRC/portable-simd
-    echo && echo "## Testing portable-simd (strict provenance)" && echo
-    MIRIFLAGS="-Zmiri-strict-provenance" \
-      cargo miri test --lib --tests
-    echo && echo "## Testing portable-simd docs (strict provenance)" && echo
-    MIRIFLAGS="-Zmiri-strict-provenance" \
-      cargo miri test --doc
-    ;;
-more)
-    cd more_tests
-    echo && echo "## Testing more" && echo
-    MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-strict-provenance" \
-      cargo miri test
-    ;;
 *)
-    echo "Unknown command"
-    exit 1
+    exit 0
 esac
